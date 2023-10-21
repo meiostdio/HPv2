0265f66fd6b9fa08edd2611fb41933a1924a3acd
Akamine2001 <Akamine5919@gmail.com>
2023-10-22 03:42:47 +0900

    Implement vercel serverless functions

--- HP/api/getGithubToken.js
+++ HP/api/getGithubToken.js
@@ -1,7 +1,7 @@
 // /api/getGithubToken.js
 module.exports = (req, res) => {
     // 環境変数からGitHub APIトークンを取得
-    const token = process.env.API_TEST;
+    const token = process.env.GITHUB_API_TOKEN;
     
     // ステータスコード200で、トークンを含むJSONを返す
     res.status(200).json({ token });
