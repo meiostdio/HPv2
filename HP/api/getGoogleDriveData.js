2ba08e018281cd9ffca7f88e84fb79f4c7b16792
Akamine2001 <Akamine5919@gmail.com>
2023-11-10 13:01:55 +0900

    update client js & serverless func

--- HP/api/getGoogleDriveData.js
+++ HP/api/getGoogleDriveData.js
@@ -1,61 +1,57 @@
-const { google } = require('googleapis');
-
 module.exports = async (req, res) => {
-  // クライアントの初期化
-  const client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    'https://meiostdio.vercel.app/api/getGoogleDriveData'
-  );
+    const {google} = require('googleapis');
 
-  // 認証フローの設定
-  const SCOPES = ['https://www.googleapis.com/auth/drive.readonly'];
-  const authUrl = client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
+    const REDIRECT_URI = "https://meiostdio.vercel.app/";
+    const oauth2client = new google.auth.OAuth2(
+        process.env.GOOGLE_CLIENT_ID,
+        process.env.GOOGLE_CLIENT_SECRET,
+        REDIRECT_URI
+    )
+    
+    oauth2client.setCredentials({
+        access_token: process.env.GOOGLE_ACCESS_TOKEN,
+        refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+    })
 
-  // ディレクトリとファイル名を引数から取得
-  const directory = req.query.directory; // ディレクトリ名
-  const fileName = req.query.fileName; // ファイル名
 
-  try {
-    // 認証フローの一部として、認証トークンを取得
-    const code = req.query.code; // 認証コード（クライアントからのリダイレクトで取得）
-    const { tokens } = await client.getToken(code);
 
-    // 取得した認証トークンをセット
-    client.setCredentials(tokens);
+    try {
+        const drive = google.drive({version: 'v3', auth: oauth2client});
+        const response = await drive.files.list();
+       } catch (error) {
+        if (error.code === 401) {
+          const newToken = await oauth2Client.getAccessToken();
+          oauth2Client.setCredentials({
+            access_token: newToken.token,
+          });
+        }
+       }
+    const drive = google.drive({version: 'v3', auth: oauth2client});
 
-    // Google Drive APIクライアントの初期化
-    const drive = google.drive({
-      version: 'v3',
-      auth: client,
+    const FOLDER_NAME = req.query.directoryName;
+    const FILE_NAME = req.query.fileName;
+    console.log('dire Name :', FOLDER_NAME);
+    console.log('file name :', FILE_NAME);
+
+    const folderResponse = await drive.files.list({
+        q: `mimeType='application/vnd.google-apps.folder' and name='${FOLDER_NAME}'`,
+        fields: 'files(id, name)',
+      });
+      const folderId = folderResponse.data.files[0].id;
+    
+    const fileResponse = await drive.files.list({
+    q: `'${folderId}' in parents and name='${FILE_NAME}' and trashed = false`,
+    fields: 'files(id, name)',
     });
+    const files = fileResponse.data.files;
 
-    // ファイルの検索
-    const files = await drive.files.list({
-      q: `name='${fileName}' and '${directory}' in parents`,
-    });
-
-    if (files.data.files.length === 0) {
-      res.status(404).send('File not found');
+    if (files.length) {
+        console.log('Files:');
+        files.forEach((file) => {
+        console.log(`${file.name} (${file.id})`);
+        });
+        res.status(200).json(files);
     } else {
-      // 最初の一致するファイルを取得
-      const file = files.data.files[0];
-
-      // ファイルのダウンロード
-      const fileData = await drive.files.get(
-        { fileId: file.id, alt: 'media' },
-        { responseType: 'stream' }
-      );
-
-      // 画像データをクライアントに送信
-      res.setHeader('Content-Type', file.mimeType);
-      fileData.data.pipe(res);
-    }
-  } catch (error) {
-    console.error('Error retrieving file:', error);
-    res.status(500).send('Error retrieving file');
-  }
-};
+        console.log('No files found.');
+    }  
+}
--- HP/js/google_drive_api.js
+++ HP/js/google_drive_api.js
@@ -1,6 +1,6 @@
-async function getGoogleDriveData(directory, fileName) {
+async function getGoogleDriveData(directoryName, fileName) {
     try {
-      const response = await fetch(`/api/getGoogleDriveData?directory=${directory}&fileName=${fileName}`);
+      const response = await fetch(`/api/getGoogleDriveData?directoryName=${encodeURIComponent(directoryName)}&fileName=${encodeURIComponent(fileName)}`);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
@@ -15,69 +15,10 @@ async function getGoogleDriveData(directory, fileName) {
   
 
 export function searchImageInFolder(){
-    const directory = document.getElementById('directoryName').value;
+    const directoryName = document.getElementById('directoryName').value;
     const fileName = document.getElementById('fileName').value;
-    console.log('directory Name is:',directory);
+    console.log('directory Name is:',directoryName);
     console.log('file Name is :', fileName);
-    getGoogleDriveData(directory,fileName);
+    getGoogleDriveData(directoryName,fileName);
 }
 window.searchImageInFolder = searchImageInFolder;
-// async function getGoogleToken(){
-//     try{
-//         const response = await fetch('/api/getGoogleToken');
-//         const data = await response.json();
-//         folderiD = data.folderid;
-//         apikey = data.apikey;
-//         clientid = data.clientid;
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// }
-
-// function initClient() {
-//     gapi.client.setApiKey(apikey);
-//     gapi.client.load('drive', 'v3').then(() => {
-//     });
-//   }
-
-// export function searchImageInFolder() {
-//     const imageName = document.getElementById('imageName').value;
-//     gapi.client.drive.files.list({
-//         q: `'${folderiD}' in parents and name='${imageName}'`,
-//         fields: 'files(id, name, webContentLink)'
-//     }).then((response) => {
-//         const files = response.result.files;
-//         if (files && files.length > 0) {
-//         loadDriveImage(files[0].id);
-//         } else {
-//         console.log('No files found.');
-//         }
-//     });
-// }
-
-// function loadDriveImage(fileId) {
-//     var request = gapi.client.drive.files.get({
-//     'fileId': fileId,
-//     'fields': 'webContentLink'
-// });
-
-// request.execute((resp) => {
-//     var downloadUrl = resp.webContentLink;
-//     document.getElementById('driveImage').src = downloadUrl;
-// });
-// }
-
-// window.searchImageInFolder = searchImageInFolder;
-// let folderiD = null;
-// let apikey = null;
-// let clientid = null;
-// try{
-//     await getGoogleToken();
-//     gapi.load('client', function(){ 
-//         initClient()
-//     }
-//     );
-// } catch (error) {
-//     console.log('Faild to get Google API token:', error);
-// }
-
