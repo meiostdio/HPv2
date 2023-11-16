eafcd2fd05ee370273db33edfe4d5cc692c01e19
Akamine2001 <Akamine5919@gmail.com>
2023-11-17 07:19:34 +0900

    Update article list func

--- HP/main.js
+++ HP/main.js
@@ -3,18 +3,35 @@ import { getArtList } from "./GithubData.js";
 // ページ読み込み完了後に行う処理
 // 記事のリストを読み込む関数を実行
 window.onload = async function() {
-  const artList = await getArtList();
-  console.log(artList);
+  try {
+    const artList = await getArtList();
 
-  let title = artList.article1.title;
-  let date = artList.article1.date;
-  let tag = artList.article1.tag;
+    let main = document.getElementById("main");
+    // ローディング表示を削除
+    main.innerHTML = '';
 
-  let main = document.getElementById("main");
-  
-  main.innerHTML = `
-    <h1>${title}</h1>
-    <p>Date: ${date}</p>
-    <p>Tags: ${tag.join(', ')}</p>
-  `;
+    // 記事のキーを取得し、逆順でループ
+    // DOMに記事を追加
+    const articleKeys = Object.keys(artList).reverse();
+    for (const key of articleKeys) {
+      const article = artList[key];
+      let title = article.title;
+      let date = article.date;
+      let tag = article.tag;
+
+      let articleDiv = document.createElement('div');
+      articleDiv.innerHTML = `
+        <div class="article">
+          <h1>${title}</h1>
+          <p>投稿日: ${date}</p>
+          <p>タグ: ${tag.join(', ')}</p>
+          <hr />
+        </div>
+        `; // 各記事の間に区切り線を追加
+
+      main.appendChild(articleDiv);
+    }
+  } catch (error) {
+    console.error("Error fetching or processing data:", error);
+  }
 };
