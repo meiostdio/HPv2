2ad04a6992d1044961b809794ea61ef500c4283f
Akamine2001 <Akamine5919@gmail.com>
2023-11-17 13:43:20 +0900

    edit article.json

--- HP/articleViewer.js
+++ HP/articleViewer.js
@@ -17,4 +17,28 @@ window.onload = async function() {
     decodedData = utf8Decoder.decode(new Uint8Array(decodedData.split('').map(c => c.charCodeAt(0))));
     let formattedData = JSON.parse(decodedData);
     console.log(formattedData);
+
+    // 表示領域を取得しローディング表示を削除
+    let main = document.getElementById('main');
+    main.innerHTML = '';
+
+    // タイトル、タグ、日付を取得して表示
+    const title = formattedData.title;
+    const tag = formattedData.tag;
+    const date = formattedData.date;
+    main.innerHTML = `
+        <h2>${title}</h2>
+        <p class="tag">${tag}</p>
+        <p>作成日時:${date}<p>
+    `;
+
+    // JSONのセクション分繰り返し、動的に表示
+    const articleKeys = Object.keys(formattedData).reverse();
+    for (const key of articleKeys) {
+        const section = formattedData[key];
+        console.log(section);
+        console.log(section.constent1);
+    }
+
+
 };
